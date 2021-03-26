17803a77193b7cbab0479a0c19bedd515808998e
James Parker <jsparker512@gmail.com>
2021-03-26 21:12:36 +1100

    Add polybar multimonitor settings

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
+# Launch polybar on all monitors
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    export MONITOR=$m
+    polybar bottom -c ~/.config/polybar/config-bottom.ini &
+done
